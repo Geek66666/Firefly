69d193a705037c949e9cc635f6fcf3d6226d121a
Geek66666 <61675035+Geek66666@users.noreply.github.com>
2026-02-13 09:06:45 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -28,13 +28,13 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/Geek66666",
 				external: true,
 				icon: "fa7-brands:github",
 			},
 			{
 				name: "Bilibili",
-				url: "https://space.bilibili.com/38932988",
+				url: "https://space.bilibili.com/1242825367",
 				external: true,
 				icon: "fa7-brands:bilibili",
 			},
